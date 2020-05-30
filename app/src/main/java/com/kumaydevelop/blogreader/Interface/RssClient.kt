20b91fd733ad7c2a818e25aca59f9708af71dbc4
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-05-30 19:40:37 +0900

    http communication style changed from rxjava to use coroutine

--- app/src/main/java/com/kumaydevelop/blogreader/Interface/RssClient.kt
+++ app/src/main/java/com/kumaydevelop/blogreader/Interface/RssClient.kt
@@ -2,6 +2,8 @@ package com.kumaydevelop.blogreader.Interface
 
 import com.kumaydevelop.blogreader.Entity.BlogEntity
 import io.reactivex.Observable
+import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -10,4 +12,7 @@ interface RssClient {
 
     @GET("{rssPath}")
     fun get(@Path("rssPath") path: String) : Observable<BlogEntity>
+
+    @GET("{rssPath}")
+    suspend fun getRss(@Path("rssPath") path: String) : BlogEntity
 }
